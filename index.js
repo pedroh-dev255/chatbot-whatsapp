e4b834ee6df6cba579d32693d86f7e3409441231
Pedro Henrique <henriquepedro1912@gmail.com>
2023-09-11 17:34:33 -0300

    adicionado percistencia na sessão do whatsapp-web

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, NoAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { configureBotCommands } = require('./src/comandos');
+const storage = require('node-persist');
 
 const client = new Client({
   authStrategy: new NoAuth(),
@@ -10,10 +11,30 @@ client.on('qr', qr => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-  console.log('Client is ready!');
+// Load the saved session if available
+storage.init().then(async () => {
+  const sessionData = await storage.getItem('whatsapp-session');
+  if (sessionData) {
+    client.applySession(sessionData);
+  }
+
+  client.on('ready', () => {
+    console.log('Client is ready!');
+  });
+
+  client.initialize();
+
+  configureBotCommands(client);
 });
 
-client.initialize();
+// Save the authentication session when it changes
+client.on('authenticated', session => {
+  storage.setItem('whatsapp-session', session);
+});
 
-configureBotCommands(client);
+// Save the authentication session when it logs out
+client.on('disconnected', reason => {
+  if (reason === 'disconnected' || reason === 'replaced') {
+    storage.removeItem('whatsapp-session');
+  }
+});
